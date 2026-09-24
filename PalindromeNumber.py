cfb66bea1d517d58c04bc29257047db167546729
Silas Sousa <sidneyc.aragao@gmail.com>
2026-09-24 20:23:35 -0300

    Correct the PalindromeNUmber

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
         if x < 0: # number negatives isn't palindrome
             return False
-        elif x % 11 == 0: # every number that your division is exectly by 11 without rest is palindrome
+        elif str(x) == str(x)[::-1]: # change to string and compare your inverse
             return True
         else: # else  isn"t palindrome
             return False
